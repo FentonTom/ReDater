c3e09ff3cbdbb79a6c8cf56aa7e3b589e0e836be
FentonTom <tom.fenton@gmail.com>
2017-02-27 10:23:04 -0800

    Weekly checkin

--- ESXiReDater_v04.ps1
+++ ESXiReDater_v04.ps1
@@ -1,4 +1,4 @@
-## This PowerShell script will replace the dates of a Linux log file with current dates
+## This PowerShell script will replace the dates of an ESXi log file with current dates.
 ## It extracts the date of the last entry in a file converts it to the current date and time
 ## and then updates all other dates in the file using that date as an ending point.
 ## v01 Created by Tom Fenton 1/31/2017
@@ -7,20 +7,20 @@
 
 #Parse paramenters passed to this script or use defaults
  param(
-    [string]$LogFile = "./auth01.log",
-    [string]$NewLogFile = "./ReAuth.log"
+    [string]$LogFile = "./vmkernel.log",
+    [string]$NewLogFile = "./ESXiNewLog"
  )
 Write-Host " Log file to be read is $LogFile " -Foreground Green
 Write-Host " Log file to be written is $NewLogFile " -Foreground Green
 
 $CheckFile = Test-Path $LogFile -PathType Leaf
-if ($CheckFile)
+if ($CheckFile) 
 {
-        Write-Host "File exist $Logfile"
-} else
+	Write-Host "File exist $Logfile"
+} else 
 {
-        Write-Host "File does not exist $Logfile"
-        exit
+	Write-Host "File does not exist $Logfile"
+	exit
 }
 
 Write-Host " Log file to be read is $LogFile " -Foreground Green
@@ -29,7 +29,7 @@ $NumLines = 0
 # Remove the old ne log file if needed
 If (Test-Path $NewLogFile)
 {
-        Remove-Item $NewLogFile
+	Remove-Item $NewLogFile
 }
 
 $content = Get-Content $LogFile
@@ -37,12 +37,25 @@ $DTemplate = 'MMM d H:m:s'
 # store the current time
 $CurTime = Get-Date
 $CurTimeS = Get-Date -Format "MMM %d %H:%m:%s"
+
+
 # Get last line in file and extract date 
 $LastDataRow = (Get-Content $LogFile -Last 1)
-$LastRowTimeS = $LastDataRow.substring(0,15)
-$LastRowTime = [datetime]::parseExact($LastRowTimeS, $DTemplate, $null)
+
+$DayT = 'yyyy-MM-dd'
+$TimeT = 'HH:mm:ss'
+$LastRowDayS = $LastDataRow.substring(0,10)
+$LastRowTimeS = $LastDataRow.substring(11,8)
+$LastRowDayTimeS = "$LastRowDayS $LastRowTimeS"
+$LastRowTime = Get-Date -date "$LastRowDayTimeS"
+
 # Compute delta date from file and current time
 $DeltaTime = $CurTime - $LastRowTime
+
+Write-Host " last row time is $LastRowTime "
+Write-Host " delta time is $DeltaTime "
+Write-Host " delta time is $DeltaTimeS "
+
 
 ## Loop through each line in the file and change the time 
 $t1 = Measure-Command {
@@ -50,25 +63,30 @@ foreach ($line in $content)
 {
 	$NumLines++ 
 	# seperate the time and messages the line in syslog
-	$var1=$line.substring(0,15)
-	$var2=$line.substring(15)
 	# convert the time string to a datetime 
-	$LineTimeS = [datetime]::parseExact($var1, $DTemplate, $null)
-	$LineTime = Get-Date -date "$LineTimeS"
+        $RowDayS = $line.substring(0,10)
+        $RowTimeS = $line.substring(11,8)
+        $var2=$line.substring(20)
+        $RowDayTimeS = "$RowDayS $RowTimeS"
+        $LineTime = Get-Date -date "$RowDayTimeS"
+
 # Compute Delta from last entry in log and current line
 	$TimeDiff = $LastRowTime - $LineTime
-# 	$TimeDiffS = Get-Date($TimeDiff) -Format "MMM %d %H:%m:%s"
 # Create new date by subtracting delta from line date and newtime
 	$NewTime = $CurTime - $TimeDiff
-	$NewTimeS = Get-Date($NewTime) -Format "MMM %d %H:%m:%s"
+	## $NewTimeS = Get-Date($NewTime) -Format u 
+	$NewTimeS = Get-Date($NewTime) -UFormat "%Y-%m-%dT%I:%M:%S"
+## sample date 2017-02-07T18:05:02Z syslog[4297813]: hostd probing is done
 # Write output 
-##	Write-Host "Old message is $line" -foreground yellow
+	Write-Host "Old message is $line" -foreground yellow
 	$NewLogS = "$NewTimeS$var2" 
-##	Write-Host $NewLogS -foreground green
-##	Write-Host "time differance is $TimeDiff  `n" -foreground yellow
+	Write-Host "$RowTimeS" -foreground red
+	Write-Host "$RowDayTimeS" -foreground red
+	Write-Host "New time $NewTimeS" -foreground red
+	Write-Host "$NewLogS" -foreground green
 	Add-Content $NewLogFile $NewLogS
-}
-}
+} # end of foreach loop
+} # end of time loop
 
 Write-Host " It took $t1 Total Seconds to process $NumLines Lines"
 Write-Host " Input file was $LogFile"
